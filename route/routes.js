d32c0868bbab3bb0c9c2ba7215b73c7b7307ded1
Menuka <hgmranasinghe@gmail.com>
2023-07-31 21:45:31 +0530

    new

--- route/routes.js
+++ route/routes.js
@@ -6,5 +6,8 @@ const router=express.Router();
 
 router.route('/student/login').post(studentController.loginStudentControllerFn);
 router.route('/student/create').post(studentController.createStudentControllerFn);
+router.route('/student/getAll').get(studentController.getDataConntrollerfn);
+router.route('/student/update/:id').patch(studentController.updateStudentController);
+router.route('/student/delete/:id').delete(studentController.deleteStudentController);
 
 module.exports=router;
